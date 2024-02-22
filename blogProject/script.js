a97b7eb7107d3a6ff24d1afbe5a4634b95ca6fde
David Potts <pottsdavidg@gmail.com>
2024-02-21 21:52:03 -0500

    Added event listeners for buttons
    Got rid of all the functions that I moved to my app module, going to keep that for the more "complicated" functions so that my actual script file is very easy to read. That way the code is extremely readable and easy to understand.

--- blogProject/script.js
+++ blogProject/script.js
@@ -1,15 +1,6 @@
-const app = {
+import { app } from "./data/app.js";
 
-    open: "<div class=\"article\"><div class=\"ptitle\">blog.entry(post);</div><div class=\"pbody\"><span class=\"blue pbody-init\">let</span><span class=\"gray pbody-init\">post =</span><span class=\"lime pbody-text\">\"",
-    close: "\"</span></div></div>"
-};
+document.getElementById("search").addEventListener("click", () => {
+    console.log(document.getElementById("search").dataset.mydata);
+});
 
-function update(input) {
-    let target = document.body.getElementsByClassName("blog-post");
-    target.innerHTML += input;
-    console.log("Body updated.");
-};
-
-function fetchPreviousPosts(n) {
-    return n;
-};
